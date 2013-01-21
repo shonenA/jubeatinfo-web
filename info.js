2da06cc4873caafc39f3ff13dc58788585619a16
shonenA <hitel00000@hotmail.com>
2013-01-21 23:39:32 +0900

    Initial commit * 더미 데이터 포함

---
+++ info.js
@@ -0,0 +1,185 @@
+var MusicEntry = function(name, artist) {
+    return {
+        name: name,
+        artist: artist,
+        bpm: 0,
+        BASIC: {score:0, time:'', level:0, note:0},
+        ADVANCED: {score:0, time:'', level:0, note:0},
+        EXTREME: {score:0, time:'', level:0, note:0}
+    }
+}
+
+var MusicEntries = (function() {
+    var idx = {};
+
+    return {
+        setEntry: function(name, artist, bpm, levels, notes) {
+            var key = escape(name);
+            if( 'undefined' == typeof(idx[key]) ) {
+                idx[key] = new MusicEntry(name, artist);
+            }
+            idx[key].bpm = bpm;
+            for( var d in levels ) {
+                idx[key][d].level = levels[d];
+                idx[key][d].note = notes[d];
+            }
+        },
+        setHistory: function(name, difficulty, score, time) {
+            var key = escape(name);
+            if( 'undefined' == typeof(idx[key]) ) {
+                idx[key] = new MusicEntry(name);
+            }
+            score = score.trim();
+            if( idx[key][difficulty].score < score ) {
+                idx[key][difficulty].score = score;
+                idx[key][difficulty].time = time;
+            }
+        },
+        get: function(name) {
+            return idx[escape(name)];
+        },
+        forEach: function(func) {
+            for( var i in idx ) {
+                func.call(this, idx[i]);
+            }
+        }
+    }
+})();
+
+function bindHandler() {
+    $('.search-query').click(function(event) {
+        event.preventDefault();
+    });
+}
+
+function updateEntry(entry) {
+    var levels = {
+        BASIC: entry[3],
+        ADVANCED: entry[4],
+        EXTREME: entry[5]
+    }, notes = {
+        BASIC: entry[6],
+        ADVANCED: entry[7],
+        EXTREME: entry[8]
+    }
+    MusicEntries.setEntry(entry[0], entry[1], entry[2], levels, notes);
+}
+
+function updateHistory(history) {
+    MusicEntries.setHistory(history.music, history.difficulty, history.score, history.date);
+}
+
+function addRows() {
+    MusicEntries.forEach(addRow);
+}
+
+function addCommas(nStr)
+{
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
+
+function formatScore(entry, rank) {
+    var score = entry.score;
+    if( typeof(rank) === 'undefined' ) {
+        rank = score;
+    }
+    var html = '';
+    if( 0 == rank ) {
+        html += '<div class="not-played-yet">Not played yet</div>';
+    } else {
+        html += '<div class="rank">';
+        if( rank < 500000 ) html += "E";
+        else if( rank < 700000 ) html += "D";
+        else if( rank < 800000 ) html += "C";
+        else if( rank < 850000 ) html += "B";
+        else if( rank < 900000 ) html += "A";
+        else if( rank < 950000 ) html += "S";
+        else if( rank < 980000 ) html += "SS";
+        else if( rank < 1000000 ) html += "SSS";
+        else html += "EXC";
+        html += '</div>';
+        html += '<div class="score">' + addCommas(score) + '</div>';
+    }
+    html += '<div class="level">' + entry.level + '</div>';
+    return html;
+}
+
+function formatMusicName(entry) {
+    var name = $('<div class="name">').text(entry.name);
+    var artist = $('<div class="artist">').text(entry.artist);
+    return name.add(artist);
+}
+
+function addRow(entry) {
+    var tr = $('<tr class="entry">');
+    var musicName = $('<td class="music">').html(formatMusicName(entry));
+    var bsc = $('<td class="bsc nfc">').html(formatScore(entry.BASIC));
+    var adv = $('<td class="adv nfc">').html(formatScore(entry.ADVANCED));
+    var ext = $('<td class="ext nfc">').html(formatScore(entry.EXTREME));
+    tr.append(musicName).append(bsc).append(adv).append(ext);
+
+    var trinfo = $('<tr class="entry-info">');
+    musicName = $('<td class="music">').html('BPM ' + entry.bpm + ' / 노트 수 ' + entry.BASIC.note + '-' + entry.ADVANCED.note + '-' + entry.EXTREME.note);
+    bsc = $('<td class="bsc">').html(entry.BASIC.time);
+    adv = $('<td class="adv">').html(entry.ADVANCED.time);
+    ext = $('<td class="ext">').html(entry.EXTREME.time);
+    trinfo.append(musicName).append(bsc).append(adv).append(ext);
+
+    $('.table').append(tr).append(trinfo);
+}
+
+function clearRows() {
+    $('.table tbody tr').remove();
+}
+
+
+function getData() {
+    getMusicData(getUserData);
+}
+
+function getMusicData(callback) {
+    $.get('data/list.txt', function(data) {
+        var rows = data.split('\n');
+
+        for( var i in rows ) {
+            var row = rows[i].trim();
+            if( '' == row ) {
+                continue;
+            }
+            updateEntry(row.split('\t'));
+        }
+
+        if( callback ) {
+            callback.call(this);
+        }
+    }, 'text');
+}
+
+function getUserData(callback) {
+    $.get('data/page.js', function(data) {
+        $('.user-name').text(data.user_name);
+        clearRows();
+        for( var i in data.history ) {
+            updateHistory(data.history[i]);
+        }
+        addRows();
+        if( callback ) {
+            callback.call(this);
+        }
+    }, 'json');
+}
+
+function initialize() {
+    bindHandler();
+    getData();
+}
+
+
